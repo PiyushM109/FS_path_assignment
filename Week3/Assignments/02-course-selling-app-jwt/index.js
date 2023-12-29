3ac1e58c72de72b69c4ca85046096feb2aa0c624
Piyush <piyushmore109@gmail.com>
2023-12-29 16:39:04 +0530

    Authentication using jwt

--- Week3/Assignments/02-course-selling-app-jwt/index.js
+++ Week3/Assignments/02-course-selling-app-jwt/index.js
@@ -25,7 +25,7 @@ const authenticateJwt = (req, res, next) => {
             if (err) {
                 res.status(403).json({ message: 'Forbidden' })
             } else {
-                req.user = user
+                req.user = user;
                 next()
             }
         })
@@ -57,31 +57,80 @@ app.post('/admin/login', (req, res) => {
 });
 
 
-app.post("/admin/courses",authenticateJwt,(req,res)=>{
+app.post("/admin/courses", authenticateJwt, (req, res) => {
     let course = req.body;
-    course.id = courses.length+1;
-    let isPresent = courses.some(c => c.title==course.title);
-    if(isPresent){
+    course.id = courses.length + 1;
+    let isPresent = courses.some(c => c.title == course.title);
+    if (isPresent) {
         res.json({ message: "Course already available" });
-    }else{
+    } else {
         courses.push(course);
         res.json({ message: 'Course created successfully', courseId: course.id })
     }
 })
 
-app.put("/admin/courses/:courseId",authenticateJwt,(req,res)=>{
+app.put("/admin/courses/:courseId", authenticateJwt, (req, res) => {
     let id = req.params.courseId;
     let updated = req.body;
     updated.id = Number(id);
-    courses[id-1] = updated;
+    courses[id - 1] = updated;
     res.json({ message: 'Course updated successfully' });
 })
 
-app.get("/admin/courses",authenticateJwt,(req,res)=>{
+app.get("/admin/courses", authenticateJwt, (req, res) => {
     res.send(courses);
 })
 
+//User routes
 
+app.post("/users/signup", (req, res) => {
+    const { username, password } = req.body
+    const userAlreadyExist = users.find(user => user.username === username)
+    if (userAlreadyExist) {
+        res.status(403).json({ message: 'user already exists' })
+    } else {
+        const user = { username: username, password: password, courses: [] }
+        users.push(user)
+        const token = generateJwt(user)
+        res.json({ message: 'user created successfully', token: token })
+    }
+})
+
+app.post("/users/login", (req, res) => {
+    const { username, password } = req.headers;
+    const user = users.find(user => user.username === username && user.password === password)
+    if (user) {
+        const token = generateJwt(user)
+        res.json({ message: 'Logged in successfully', token: token })
+    } else {
+        res.status(403).json({ message: "Invalid credentials" })
+    }
+
+})
+
+app.get("/users/courses", authenticateJwt, (req, res) => {
+    res.json({ courses: courses.filter(c => c.published) });
+});
+
+app.post("/users/courses/:courseId", authenticateJwt, (req, res) => {
+    const username = req.user.username
+    const id = parseInt(req.params.courseId)
+    const course = courses.find(c => c.id === id && c.published)
+    if (course) {
+        const user = users.find(u => u.username === username)
+        user.courses.push(id)
+        res.json({ message: 'Course purchased successfully' })
+    } else {
+        res.status(404).json({ message: 'Course not found' })
+    }
+})
+
+app.get('/users/purchasedCourses', authenticateJwt, (req, res) => {
+    const username = req.user.username
+    const user = users.find(u => u.username === username)
+    const purchasedCourses = courses.filter(c => user.courses.includes(c.id))
+    res.json({purchasedCourses: purchasedCourses})
+  });
 
 
 
