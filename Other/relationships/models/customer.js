d41b1c662d7b9eb5f5850d3adcb808da9fa1a3e5
Piyush <piyushmore109@gmail.com>
2024-01-01 18:18:22 +0530

    mongoose pre and post middleware tut

--- Other/relationships/models/customer.js
+++ Other/relationships/models/customer.js
@@ -15,31 +15,70 @@ const customerSchema = new mongoose.Schema(
     {
         name: String,
         orders: [
-            {
+            {  
                 type: mongoose.Schema.Types.ObjectId,
                 ref: "Order"
             }
         ]
     }
 )
+
+// customerSchema.pre("findOneAndDelete", async ()=>{
+//     console.log("Pre middleware")
+// })
+
+customerSchema.post("findOneAndDelete", async (customer)=>{
+    if(customer.orders.length){
+        let res = await Order.deleteMany({ _id: {$in : customer.orders }});
+        console.log(res)
+    }
+})
+
+
 const Order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer",customerSchema);
 
 
 const addCustomer = async ()=>{
     let cust1 = new Customer({
-        name: " Virat Kohli",
+        name: "ankush More",
     });
 
-    let order1 = await Order.findOne({item:"chips"});
-    let order2 = await Order.findOne({item:"Chocolate"});
+    let order1 = new Order({
+        item : "pizza",
+        price :250
+    });
 
     cust1.orders.push(order1);
-    cust1.orders.push(order2);
-
+    await order1.save();
     let res = await cust1.save();
     console.log(res);
 }
+
+
+// addCustomer();
+
+const delCust = async ()=>{
+    let data = await Customer.findByIdAndDelete("658fe92ae981677ad5b07acd")
+    console.log(data)
+}
+
+delCust();
+
+// const addCustomer = async ()=>{
+//     let cust1 = new Customer({
+//         name: " Virat Kohli",
+//     });
+
+//     let order1 = await Order.findOne({item:"chips"});
+//     let order2 = await Order.findOne({item:"Chocolate"});
+
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);
+
+//     let res = await cust1.save();
+//     console.log(res);
+// }
 
 // const addOrders = async () => {
 //     let res = await Order.insertMany([
@@ -58,4 +97,4 @@ const addCustomer = async ()=>{
 //     console.log(res);
 // }
 
-addCustomer();
+// addCustomer();
