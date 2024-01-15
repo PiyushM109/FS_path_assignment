9845b72d56e122b4188a79a45a955052ada402ca
Piyush <piyushmore109@gmail.com>
2024-01-15 16:44:12 +0530

    Basics of react

---
+++ Other/viteReact/basic-react/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import ProductList from "./ProductList";
+
+
+
+function Description() {
+  return <h4>This is description from Description component</h4>
+}
+function App() {
+  return (
+  <ProductList />
+  );
+}
+
+export default App
